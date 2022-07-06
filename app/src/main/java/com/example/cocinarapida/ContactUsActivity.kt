8800770da8f7513abfb68a3b25aaa2fc66dd4c3b
Emmanuel <emmanuelmosa@gmail.com>
2022-07-05 22:12:14 -0500

    Boton de Back

--- app/src/main/java/com/example/cocinarapida/ContactUsActivity.kt
+++ app/src/main/java/com/example/cocinarapida/ContactUsActivity.kt
@@ -2,10 +2,25 @@ package com.example.cocinarapida
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.example.cocinarapida.databinding.ActivityContactUsBinding
+import com.example.cocinarapida.databinding.ActivityDesayunosBinding
 
 class ContactUsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityContactUsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_contact_us)
+        binding = ActivityContactUsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home){
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
