76f59de9bd7945b502e184de584ce31540d42148
Emmanuel <emmanuelmosa@gmail.com>
2022-07-12 18:48:49 -0500

    agreagando toas

--- app/src/main/java/com/example/cocinarapida/RecipeTemplateActivity.kt
+++ app/src/main/java/com/example/cocinarapida/RecipeTemplateActivity.kt
@@ -9,8 +9,6 @@ import com.example.cocinarapida.databinding.ActivityRecipeTemplateBinding
 
 class RecipeTemplateActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRecipeTemplateBinding
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRecipeTemplateBinding.inflate(layoutInflater)
@@ -38,7 +36,6 @@ class RecipeTemplateActivity : AppCompatActivity() {
         binding.ingredient10.text = intent.extras?.getString("ingredient_10")
         binding.ingredient11.text = intent.extras?.getString("ingredient_11")
 
-
         binding.substitute1.text = intent.extras?.getString("sustituto_1")
         binding.substitute2.text = intent.extras?.getString("sustituto_2")
 
@@ -50,6 +47,64 @@ class RecipeTemplateActivity : AppCompatActivity() {
         binding.ingredient1.setOnClickListener {
            listaCompras()
         }
+
+        if (binding.ingredient1.text.isBlank()){
+            binding.ingredient1.visibility = View.GONE
+        }else{
+            binding.ingredient1.visibility = View.VISIBLE
+        }
+        if (binding.ingredient2.text.isBlank()){
+            binding.ingredient2.visibility = View.GONE
+        }else{
+            binding.ingredient2.visibility = View.VISIBLE
+        }
+        if (binding.ingredient3.text.isBlank()){
+            binding.ingredient3.visibility = View.GONE
+        }else{
+            binding.ingredient3.visibility = View.VISIBLE
+        }
+        if (binding.ingredient4.text.isBlank()){
+            binding.ingredient4.visibility = View.GONE
+        }else{
+            binding.ingredient4.visibility = View.VISIBLE
+        }
+        if (binding.ingredient5.text.isBlank()){
+            binding.ingredient5.visibility = View.GONE
+        }else{
+            binding.ingredient5.visibility = View.VISIBLE
+        }
+        if (binding.ingredient6.text.isBlank()){
+            binding.ingredient6.visibility = View.GONE
+        }else{
+            binding.ingredient6.visibility = View.VISIBLE
+        }
+        if (binding.ingredient7.text.isBlank()){
+            binding.ingredient7.visibility = View.GONE
+        }else{
+            binding.ingredient7.visibility = View.VISIBLE
+        }
+        if (binding.ingredient8.text.isBlank()){
+            binding.ingredient8.visibility = View.GONE
+        }else{
+            binding.ingredient8.visibility = View.VISIBLE
+        }
+        if (binding.ingredient9.text.isBlank()){
+            binding.ingredient9.visibility = View.GONE
+        }else{
+            binding.ingredient9.visibility = View.VISIBLE
+        }
+        if (binding.ingredient10.text.isBlank()){
+            binding.ingredient10.visibility = View.GONE
+        }else{
+            binding.ingredient10.visibility = View.VISIBLE
+        }
+        if (binding.ingredient11.text.isBlank()){
+            binding.ingredient11.visibility = View.GONE
+        }else{
+            binding.ingredient11.visibility = View.VISIBLE
+        }
+
+
 
 
 
