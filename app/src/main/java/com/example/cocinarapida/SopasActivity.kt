352ca5ba8976ff08a9e0d86ba7ffd1b332b4b281
Emmanuel <emmanuelmosa@gmail.com>
2022-07-15 17:44:52 -0500

    Añadiendo recetas en desayunos

--- app/src/main/java/com/example/cocinarapida/SopasActivity.kt
+++ app/src/main/java/com/example/cocinarapida/SopasActivity.kt
@@ -1,10 +1,9 @@
 package com.example.cocinarapida
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.cocinarapida.databinding.ActivitySopasBinding
 
 class SopasActivity : AppCompatActivity() {
@@ -18,43 +17,43 @@ class SopasActivity : AppCompatActivity() {
 
         binding.btSopaFideos.setOnClickListener {
 
-            val sop_fod_ing_1: String = getString(R.string.sal_gusto)
-            val sop_fod_ing_2: String = getString(R.string.bolsa_sop_fideos)
-            val sop_fod_ing_3: String = getString(R.string.aceite)
-            val sop_fod_ing_4: String = getString(R.string.pure_sazonado_tetrapack)
-            val sop_fod_ing_5: String = getString(R.string.caldo_pollo)
+            val ingredient1: String = getString(R.string.sal_gusto)
+            val ingredient2: String = getString(R.string.bolsa_sop_fideos)
+            val ingredient3: String = getString(R.string.aceite)
+            val ingredient4: String = getString(R.string.pure_sazonado_tetrapack)
+            val ingredient5: String = getString(R.string.caldo_pollo)
 
-            val sop_fod_subs_1: String = getString(R.string.caldo_pollo_tp)
+            val substitute1: String = getString(R.string.caldo_pollo_tp)
 
-            val pre_sop_fideos: String = getString(R.string.prep_sop_fideos)
-            openSopaFideosActivity(sop_fod_ing_1, sop_fod_ing_2, sop_fod_ing_3, sop_fod_ing_4,
-                                   sop_fod_ing_5, sop_fod_subs_1, pre_sop_fideos)
+            val preparation1: String = getString(R.string.prep_sop_fideos)
+
+            openSopaFideosActivity(ingredient1, ingredient2, ingredient3, ingredient4, ingredient5,
+                                   substitute1, preparation1)
         }
 
         binding.btCaldoPollo.setOnClickListener {
 
-            val cal_pollo_ing_1: String = getString(R.string.piernas_pollo_4)
-            val cal_pollo_ing_2: String = getString(R.string.muslos_pollo_4)
-            val cal_pollo_ing_3: String = getString(R.string.zanahorias_4)
-            val cal_pollo_ing_4: String = getString(R.string.chayotes_2)
-            val cal_pollo_ing_5: String = getString(R.string.papas_2)
-            val cal_pollo_ing_6: String = getString(R.string.brocoli)
-            val cal_pollo_ing_7: String = getString(R.string.calabazas_4)
-            val cal_pollo_ing_8: String = getString(R.string.cebolla)
-            val cal_pollo_ing_9: String = getString(R.string.dientes_ajo_2)
-            val cal_pollo_ing_10: String = getString(R.string.manojo_cilantro)
-            val cal_pollo_ing_11: String = getString(R.string.sal_grano)
+            val ingredient1: String = getString(R.string.piernas_pollo_4)
+            val ingredient2: String = getString(R.string.muslos_pollo_4)
+            val ingredient3: String = getString(R.string.zanahorias_4)
+            val ingredient4: String = getString(R.string.chayotes_2)
+            val ingredient5: String = getString(R.string.papas_2)
+            val ingredient6: String = getString(R.string.brocoli)
+            val ingredient7: String = getString(R.string.calabazas_4)
+            val ingredient8: String = getString(R.string.cebolla)
+            val ingredient9: String = getString(R.string.dientes_ajo_2)
+            val ingredient10: String = getString(R.string.manojo_cilantro)
+            val ingredient11: String = getString(R.string.sal_grano)
 
-            val cal_pollo_subs_1: String = getString(R.string.vegetales_congelados)
+            val substitute1: String = getString(R.string.vegetales_congelados)
 
-            val cal_pollo_prep: String = getString(R.string.caldo_pollo_preparation)
+            val preparation1: String = getString(R.string.caldo_pollo_preparation)
 
-            val cal_pollo_option: String = getString(R.string.optional_caldo_pollo)
+            val optional: String = getString(R.string.optional_caldo_pollo)
 
-            openCaldoPolloActivity(cal_pollo_ing_1, cal_pollo_ing_2, cal_pollo_ing_3, cal_pollo_ing_4,
-                                   cal_pollo_ing_5, cal_pollo_ing_6, cal_pollo_ing_7, cal_pollo_ing_8,
-                                   cal_pollo_ing_9, cal_pollo_ing_10, cal_pollo_ing_11, cal_pollo_subs_1,
-                                    cal_pollo_prep, cal_pollo_option)
+            openCaldoPolloActivity(ingredient1, ingredient2, ingredient3, ingredient4, ingredient5,
+                ingredient6, ingredient7, ingredient8, ingredient9, ingredient10, ingredient11, substitute1,
+                preparation1, optional)
         }
 
         binding.btSopaVerdolagas.setOnClickListener {
@@ -460,54 +459,53 @@ class SopasActivity : AppCompatActivity() {
         startActivity(intent)
     }
 
-    private fun openCaldoPolloActivity(cal_pollo_ing_1: String, cal_pollo_ing_2: String, cal_pollo_ing_3: String,
-                                       cal_pollo_ing_4: String, cal_pollo_ing_5: String, cal_pollo_ing_6: String,
-                                       cal_pollo_ing_7: String, cal_pollo_ing_8: String, cal_pollo_ing_9: String,
-                                       cal_pollo_ing_10: String, cal_pollo_ing_11: String, cal_pollo_subs_1: String,
-                                       cal_pollo_prep: String, cal_pollo_option: String) {
+    private fun openCaldoPolloActivity(ingredient1: String, ingredient2: String, ingredient3: String,
+    ingredient4: String, ingredient5: String, ingredient6: String, ingredient7: String, ingredient8: String,
+    ingredient9: String, ingredient10: String, ingredient11: String, substitute1: String, preparation1: String,
+    optional1: String) {
 
         val intent = Intent(this, RecipeTemplateActivity::class.java)
 
         intent.putExtra("img_top_recipe", R.drawable.caldo_pollo_2)
 
-        intent.putExtra("ingredient_1", cal_pollo_ing_1)
-        intent.putExtra("ingredient_2", cal_pollo_ing_2)
-        intent.putExtra("ingredient_3", cal_pollo_ing_3)
-        intent.putExtra("ingredient_4", cal_pollo_ing_4)
-        intent.putExtra("ingredient_5", cal_pollo_ing_5)
-        intent.putExtra("ingredient_6", cal_pollo_ing_6)
-        intent.putExtra("ingredient_7", cal_pollo_ing_7)
-        intent.putExtra("ingredient_8", cal_pollo_ing_8)
-        intent.putExtra("ingredient_9", cal_pollo_ing_9)
-        intent.putExtra("ingredient_10", cal_pollo_ing_10)
-        intent.putExtra("ingredient_11", cal_pollo_ing_11)
+        intent.putExtra("ingredient_1", ingredient1)
+        intent.putExtra("ingredient_2", ingredient2)
+        intent.putExtra("ingredient_3", ingredient3)
+        intent.putExtra("ingredient_4", ingredient4)
+        intent.putExtra("ingredient_5", ingredient5)
+        intent.putExtra("ingredient_6", ingredient6)
+        intent.putExtra("ingredient_7", ingredient7)
+        intent.putExtra("ingredient_8", ingredient8)
+        intent.putExtra("ingredient_9", ingredient9)
+        intent.putExtra("ingredient_10", ingredient10)
+        intent.putExtra("ingredient_11", ingredient11)
 
-        intent.putExtra("sustituto_1", cal_pollo_subs_1)
+        intent.putExtra("sustituto_1", substitute1)
 
-        intent.putExtra("preparation1", cal_pollo_prep)
+        intent.putExtra("preparation1", preparation1)
 
-        intent.putExtra("optional_1", cal_pollo_option)
+        intent.putExtra("optional_1", optional1)
 
         startActivity(intent)
     }
 
-    private fun openSopaFideosActivity(sop_fod_ing_1: String,sop_fod_ing_2: String,sop_fod_ing_3:
-                                        String,sop_fod_ing_4: String,sop_fod_ing_5: String,
-                                       sop_fod_subs_1: String, pre_sop_fideos: String) {
+    private fun openSopaFideosActivity(ingredient1: String, ingredient2: String, ingredient3: String,
+                                       ingredient4: String, ingredient5: String,
+                                       substitute1: String, preparation1: String) {
 
         val intent = Intent(this, RecipeTemplateActivity::class.java)
 
         intent.putExtra("img_top_recipe", R.drawable.sopa_fideos)
 
-        intent.putExtra("ingredient_1", sop_fod_ing_1)
-        intent.putExtra("ingredient_2", sop_fod_ing_2)
-        intent.putExtra("ingredient_3", sop_fod_ing_3)
-        intent.putExtra("ingredient_4", sop_fod_ing_4)
-        intent.putExtra("ingredient_5", sop_fod_ing_5)
+        intent.putExtra("ingredient_1", ingredient1)
+        intent.putExtra("ingredient_2", ingredient2)
+        intent.putExtra("ingredient_3", ingredient3)
+        intent.putExtra("ingredient_4", ingredient4)
+        intent.putExtra("ingredient_5", ingredient5)
 
-        intent.putExtra("sustituto_1", sop_fod_subs_1)
+        intent.putExtra("sustituto_1", substitute1)
 
-        intent.putExtra("preparation1", pre_sop_fideos)
+        intent.putExtra("preparation1", preparation1)
 
         startActivity(intent)
     }
