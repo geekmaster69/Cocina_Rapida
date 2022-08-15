1215d85fcff19f7b6964f48fbe2b21cdaf257362
Emmanuel <emmanuelmosa@gmail.com>
2022-08-15 18:57:38 -0500

    Mejorando lista de compras

--- app/src/main/java/com/example/cocinarapida/ContactUsActivity.kt
+++ app/src/main/java/com/example/cocinarapida/ContactUsActivity.kt
@@ -1,5 +1,7 @@
 package com.example.cocinarapida
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
@@ -14,6 +16,26 @@ class ContactUsActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.btRecetaplus.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("albeitarray@gmail.com"))
+                putExtra(Intent.EXTRA_SUBJECT, "Sugerencia de receta")
+                putExtra(Intent.EXTRA_TEXT, "Hola!!! Me encantaria que agregaran la siguiente receta...")
+            }
+            startActivity(intent)
+        }
+
+        binding.btSugerencia.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("albeitarray@gmail.com"))
+                putExtra(Intent.EXTRA_SUBJECT, "Sugerencia de apliacacion")
+                putExtra(Intent.EXTRA_TEXT, "Hola!!! Tengo una sugerencia para su aplicacion...")
+            }
+            startActivity(intent)
+        }
     }
 
 
