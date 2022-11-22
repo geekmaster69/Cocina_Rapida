1a3d2982815c35c61056569b4c315ca377418bdb
Emmanuel <emmanuelmosa@gmail.com>
2022-11-21 19:03:08 -0600

    Agregando recetas

--- app/src/main/java/com/example/cocinarapida/AgradecimientoActivity.kt
+++ app/src/main/java/com/example/cocinarapida/AgradecimientoActivity.kt
@@ -2,6 +2,7 @@ package com.example.cocinarapida
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import com.example.cocinarapida.databinding.ActivityAgradecimientoBinding
 
 class AgradecimientoActivity : AppCompatActivity() {
@@ -10,5 +11,13 @@ class AgradecimientoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityAgradecimientoBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home){
+            onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
